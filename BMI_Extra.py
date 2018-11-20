3bac87ec4e7a23f354eb1d2e6a5864d33767da79
Wiktor Kowalczyk <kowalczyk.wiktor@gmail.com>
2018-11-20 08:09:26 +0100

    calculateBMI_withINPUT

---
+++ BMI_Extra.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+print("Podaj wzrost w cm:")
+wzrost = input()
+print("Podaj wagę w kg:")
+waga = input()
+BMI = int(waga) / int(wzrost) ** 2
+print ("Twoje BMI wynosi:", BMI)
+
