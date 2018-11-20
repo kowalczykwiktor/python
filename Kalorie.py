41b485dd0e6b9d29e076051e580376d39405d2a3
Wiktor Kowalczyk <kowalczyk.wiktor@gmail.com>
2018-11-20 08:11:35 +0100

    caloryCalculate_simple

---
+++ Kalorie.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+wzrost = 1.7
+waga = 63
+wiek = 25
+s = -161
+aktywnosc_fizyczna = 1.6
+kalorie = (10 * waga + 6.25 * wzrost - 5 * 25 + s) * aktywnosc_fizyczna
+print("Zapotrzebowanie Twojej kobiety na kalorie to:", kalorie)
