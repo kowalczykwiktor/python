1b9280ca92484d62a37eb7ee7e90d9fb9857e3e3
Wiktor Kowalczyk <kowalczyk.wiktor@gmail.com>
2020-03-04 12:20:24 +0100

    changesIntoSeleniumTests

--- seleniumSimpleTestWebDriver/seleniumSimpleTestWebDriver.py
+++ seleniumSimpleTestWebDriver/seleniumSimpleTestWebDriver.py
@@ -1,5 +1,10 @@
 # pip3 install selenium
 from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
 
-browser = webdriver.Firefox()
-browser.get('example.com')
+# browser = webdriver.Firefox()
+# browser = webdriver.Firefox(r'./SynologyDrive/Repository/Python/seleniumSimpleTestWebDriver')
+browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+browser.get('https://www.youtube.com/')
+print(browser.title)
+browser.quit()
