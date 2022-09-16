f2bb45c5ebf96e46faf95a26da765f1268a74d1d
Wiktor Kowalczyk <wiktor@Mac-Studio-Wiktor.local>
2022-09-16 12:08:49 +0200

    if_else_correction

--- PJATK/I/Zadanie_6.1.py
+++ PJATK/I/Zadanie_6.1.py
@@ -6,8 +6,6 @@ print("Ile masz lat?")
 age = int( input() )
 if ( age >= 18 ):
     print("Użytkownik pełnoletni")
-if ( age > 100 ):
-    print("200 lat")
 else:
     print("Użytkownik niepełnoletni")
-    print("Do 18. urodzin zostało", 18 - age, "lat")
+    print("Do 18 urodzin zostało:", 18 - age, "lat")
